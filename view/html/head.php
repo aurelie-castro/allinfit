2aa652f0404dcd4c65a3b34d8eab1b46be90eea4
Jessica <j.mangbau@interface3.be>
2020-10-06 14:58:33 +0200

    added nav

--- view/html/head.php
+++ view/html/head.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le site des web</title>
-    <link rel="stylesheet" href="public/css/style.css">
+    <title>AllinFit</title>
+   <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
     
